70d287b0bad406536f767441a765ba7d71a9ecaa
Lucas Ratouit <ratouitlucaspro@gmail.com>
2022-07-04 22:07:38 +0200

    add contact folder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.js";
 import Home from "./components/Home/Home.js";
+import Contact from "./components/Contact/Contact.js"
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Contacter" /*element={<Contacter />}*/ />
+        <Route path="/Contacter" element={<Contact />} />
       </Routes>
     </Router>
   );
