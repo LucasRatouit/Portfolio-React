2a4644091ae2a957999c57f7ecc2e32a7a07e112
Lucas Ratouit <ratouitlucaspro@gmail.com>
2022-07-21 21:01:32 +0200

    add CV

---
+++ src/components/CV/CV.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CV() {
+    return (
+        <section className="CV">
+            <a className="CVLien" href="./RATOUIT_Lucas_CV.pdf">Télécharger mon CV</a>
+            <img src="./RATOUIT_Lucas_CV.png" alt="CV" />
+        </section>
+
+    );
+}
+
+export default CV;
